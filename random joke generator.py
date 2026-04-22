75b2f24184d41cf803fc1b2ff6e283cbc31a28ac
JeffyGamez <259977003+JeffyGamez@users.noreply.github.com>
2026-04-22 08:42:13 -0400

    Upgrade Random Joke Generator to version 1.3
    Updated the version to 1.3 and improved the joke selection loop.

--- random joke generator.py
+++ random joke generator.py
@@ -3,22 +3,35 @@ import random
 print ("This repo was made by jeffygamez")
 print ("now loading")
 time.sleep (5)
-print ("Welcome To Random Joke Generator version 1.2.7")
+print ("Welcome To Random Joke Generator version 1.3")
 joke_1 = "Why did the chicken cross the road? To run from the KFC"
 joke_2 = "Have you heard the rumor of the margarin? Never mind I butter not spread it"
 joke_3 = "What did the eraser say to the pencil? You're lookin mighty sharp today"
 
-joke_selected = random.randint (1, 3) 
+keep_playing = "yes"
 
-print (f"Selecting Random Joke Number: {joke_selected}")
+while keep_playing == "yes" or "Yes":
+  joke_selected = random.randint (1, 3) 
 
-time.sleep (3)
+    print (f"Selecting Random Joke Number: {joke_selected}")
 
-if joke_selected == 1:
-  print (f"Random Joke Is: {joke_1}")
+    time.sleep (3)
 
-if joke_selected == 2:
-  print (f"Random Joke Is: {joke_2}")
+  if joke_selected == 1:
+    print (f"Random Joke Is: {joke_1}")
 
-if joke_selected == 3:
-  print (f"Random Joke Is: {joke_3}")
+  elif joke_selected == 2:
+    print (f"Random Joke Is: {joke_2}")
+
+  elif joke_selected == 3:
+    print (f"Random Joke Is: {joke_3}")
+  else:
+    keep_playing = input ("Would You Like To Keep Playing Yes/No")
+    if keep_playing == "Yes" or "yes":
+      print ("Thanks For using Random Joke Generator")
+    elif keep_playing == "No" or "no":
+      print ("Goodbye Thank You For Using Random Joke Generator Version 1.3 By JeffyGamez")
+
+
+
+
