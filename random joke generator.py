20a08013c430370043067115a1eea0e6ca40cce4
JeffyGamez <259977003+JeffyGamez@users.noreply.github.com>
2026-04-22 10:15:21 -0400

    Update version number and improve comments

--- random joke generator.py
+++ random joke generator.py
@@ -1,11 +1,12 @@
 import time 
 import random
-import math
+import math # the import math is not needed at the moment but i may need it for future updates
+# This Section is the into to the program
 print ("This repo was made by jeffygamez")
 print ("now loading")
 time.sleep (5)
-print ("Welcome To Random Joke Generator version 1.3.2")
-
+print ("Welcome To Random Joke Generator version 1.4")
+# This section is for all the jokes make sure to put comma's after new jokes and to format it correctly for string text
 jokes = [
 "Why did the chicken cross the road? To run from the KFC",
 "Have you heard the rumor of the margarine? Never mind I butter not spread it",
@@ -18,7 +19,7 @@ jokes = [
 "What did the ocean say to the beach? Nothing, it just waved!",
 "Why did the math book look sad? Because it had too many problems!"
 ]
-
+# This is the section that keeps the joke going
 keep_playing = "yes"
 
 while keep_playing == "yes" or keep_playing == "Yes":
@@ -29,7 +30,7 @@ while keep_playing == "yes" or keep_playing == "Yes":
   time.sleep (3)
 
   print (f"Random Joke is: {joke_selected}")  
-
+# This is the inner while loop to enshure that the user enters a suported response 
   valid_input = False
   while valid_input == False: 
     keep_playing = input ("Would You Like To Keep Playing Yes/No")
@@ -38,7 +39,7 @@ while keep_playing == "yes" or keep_playing == "Yes":
       valid_input = True
       continue
     elif keep_playing == "No" or keep_playing == "no":
-      print ("Goodbye Thank You For Using Random Joke Generator Version 1.3.2 By JeffyGamez")
+      print ("Goodbye Thank You For Using Random Joke Generator Version 1.4 By JeffyGamez") #<--- Remember to change this lines version number when changing to one
       valid_input = True
       break
     else:
