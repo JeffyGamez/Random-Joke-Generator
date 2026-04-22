57bb1dd83ebf55ddb01d33f5dc930ce1b256440e
JeffyGamez <259977003+JeffyGamez@users.noreply.github.com>
2026-04-22 09:32:34 -0400

    Refactor joke selection and input handling
    Refactor joke selection to use a list and random.choice. Simplify input handling for continuing the game.

--- random joke generator.py
+++ random joke generator.py
@@ -5,31 +5,25 @@ print ("This repo was made by jeffygamez")
 print ("now loading")
 time.sleep (5)
 print ("Welcome To Random Joke Generator version 1.3.2")
-joke_1 = "Why did the chicken cross the road? To run from the KFC"
-joke_2 = "Have you heard the rumor of the margarin? Never mind I butter not spread it"
-joke_3 = "What did the eraser say to the pencil? You're lookin mighty sharp today"
 
+jokes = [
+"Why did the chicken cross the road? To run from the KFC"
+"Have you heard the rumor of the margarin? Never mind I butter not spread it"
+"What did the eraser say to the pencil? You're lookin mighty sharp today"
+]
 keep_playing = "yes"
 
 while keep_playing == "yes" or keep_playing == "Yes":
-  joke_selected = random.randint (1, 3) 
+  joke_selected = random.choice ( jokes ) 
 
-  print (f"Selecting Random Joke Number: {joke_selected}")
-
+  print ("Selecting Random Joke.")
+ 
   time.sleep (3)
 
-  if joke_selected == 1:
-    print (f"Random Joke Is: {joke_1}")
+  print (f"Random Joke is: {joke_selected}")  
 
-  elif joke_selected == 2:
-    print (f"Random Joke Is: {joke_2}")
-
-  elif joke_selected == 3:
-    print (f"Random Joke Is: {joke_3}")
-   
-  valid_input = False 
-  while valid_input == False:
-    
+  valid_input = False:
+  while valid_input = False: 
     keep_playing = input ("Would You Like To Keep Playing Yes/No")
   
     if keep_playing == "Yes" or keep_playing == "yes":
